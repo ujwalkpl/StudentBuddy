b37ed96bff863551837df5937408ca23273b4dfc
Ujwalkpl <ujwlkpl@gmail.com>
2020-03-23 16:56:26 +0530

    changes

--- process.py
+++ process.py
@@ -45,13 +45,20 @@ for i in b:
         dayr = set(re.findall(day,i.lower()))
         timer = list(set(re.findall(time,i.lower())))
         Subjectr = set(re.findall(Subject,i.lower()))
+        Subjectstr = " ".join(Subjectr)
+        eventstr = " ".join(eventr)
+        daystr = " ".join(dayr)
+        if len(timer):
+            timestr = " ".join(timer[0])
+
         if len(eventr) and len(dayr) and len(Subjectr) :
-            print('Subject :', " ".join(Subjectr), end = ' ')
-            print(" ".join(eventr))
-            print('Day :'," ".join(dayr))
-            print('Time :'," ".join(timer[0]))
+            print('Subject :' + Subjectstr +" "+ eventstr)
             
-            print(i)
+
+            print('Day :', daystr)
+            print('Time :',timestr)
+            
+            # print(i)
 
             print()
             print()
