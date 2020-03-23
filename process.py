d23207dd2d86dd2f607dbd69078b440439cac76f
Ujwalkpl <ujwlkpl@gmail.com>
2020-03-23 18:01:23 +0530

    added aishwaryas code

--- process.py
+++ process.py
@@ -1,4 +1,5 @@
 import pickle
+from dateparser.search import search_dates
 import re
 with open("test1.txt", "rb") as fp:
     b = pickle.load(fp)
@@ -41,21 +42,27 @@ for i in b:
         day = r'(mon|tue|wed|thu|fri|sat|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec|next.*week|next.*month)'
         time = r'([0-9]:[0-9]+|[0-9]+.[0-9]+).*(am|pm)'
         Subject = r'(\bss|\bsystem software|\bml|\bmachine learning|\bjava|\bjava)'
+        date = r'(\b[0-9]+.(th|rd|th|st))'
         eventr = set(re.findall(event,i.lower())) 
         dayr = set(re.findall(day,i.lower()))
         timer = list(set(re.findall(time,i.lower())))
         Subjectr = set(re.findall(Subject,i.lower()))
+        dater = list(set(re.findall(date,i.lower())))
+        
         Subjectstr = " ".join(Subjectr)
         eventstr = " ".join(eventr)
         daystr = " ".join(dayr)
+        if len(dater):
+            datestr = "".join(dater[0][0])
         if len(timer):
             timestr = " ".join(timer[0])
 
         if len(eventr) and len(dayr) and len(Subjectr) :
+            # print(dater)
             print('Subject :' + Subjectstr +" "+ eventstr)
             
 
-            print('Day :', daystr)
+            print('Day :', daystr + " " + datestr)
             print('Time :',timestr)
             
             # print(i)
@@ -63,6 +70,14 @@ for i in b:
             print()
             print()
 
+# for s in b:
+#         event = re.findall(r'(test|lab|fee|payment)',s)
+#         dates = search_dates(s)
+#         if len(event):
+#             print(event)
+#             print(dates[0][0])
+#             print()
+#             print()
 
 
 # process()
